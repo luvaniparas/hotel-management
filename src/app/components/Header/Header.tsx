0aa6805c76f8341f8f34f9c887f69b8e4859d937
Paras Luvani <luvaniparas5@gmail.com>
2023-12-21 00:12:14 +0530

    Header Component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import { FaUserCircle } from 'react-icons/fa';
+import Link from 'next/link';
+
+function Header() {
+    return ( <div>
+        
+        <header> 
+                <Link href="/">Hotelzzz</Link>   
+                <FaUserCircle/> 
+        </header>
+
+
+        
+
+
+
+    </div> );
+}
+
+export default Header;
