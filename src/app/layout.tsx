9844daf390d9fd8343e566d7fc1e34c05e54db09
Paras Luvani <luvaniparas5@gmail.com>
2023-12-21 00:11:45 +0530

    Project Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+import Header from '#/src/app/components/Header/Header'
+
+const poppins = Poppins({ subsets: ['latin'],weight:['400','500','700','900'],style: ['italic','normal'],variable: "--font-poppins" })
+
+export const metadata: Metadata = {
+  title: 'Hotel Management App',
+  description: 'Discover best hotel rooms',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+          <main className='font-normal'>
+            <Header/>
+          {children}
+            </main>
+        </body>
+    </html>
+  )
+}
